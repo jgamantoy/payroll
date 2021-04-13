16a55f5ac9bc9495aff508048367f05d961582c7
Hippohobodude <73829842+Hippohobodude@users.noreply.github.com>
2021-04-13 15:31:02 +0800

    init Project

--- src/components/layouts/Project.js
+++ src/components/layouts/Project.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Member from '../reuseable/Member';
 
 const Project = () => {
+    const testArray = [1,2,3]
     return(
         <div className="Project">
             <div className="Project__header">
                 <h1>Project Title</h1>
             </div>
             <div className="Project__main">
-                main
+                <div className="Project__main__member">
+                    <h2>Members</h2>
+                    {testArray.map(() => {
+                        return <Member />
+                    })}
+                </div>
+                <div className="Project__main__summary">
+                    <div className="Project__main__summary__container">
+                        <h2>Total Cost</h2>
+                        <h3>Php 12,000</h3>
+                        <p>Start Date</p>
+                        <p>End Date</p>
+                        <p> 10 members</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/reuseable/Member.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Member = () => {
+    return (
+        <div className="Member">
+            <div className="Member__details">
+                <h3>Name</h3>
+                <h4>Role</h4>
+                <p>Payment date:</p>
+                <p>Php 4,000/m</p>
+            </div>
+            <div className="Member__image">
+
+            </div>
+        </div>
+    )
+}
+
+export default Member;

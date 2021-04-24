4fa8eaa3606dc754e8ef7f1562cdfecd087a0a61
Hippohobodude <73829842+Hippohobodude@users.noreply.github.com>
2021-04-24 10:50:40 +0800

    db setup

--- _Database/migrations/sqls/20210417152924-seed-up.sql
+++ _Database/migrations/sqls/20210417152924-seed-up.sql
@@ -1,4 +1,12 @@
-/* Replace with your SQL commands */
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2021 at 04:39 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.2.5
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -8,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
+CREATE DATABASE `payroll`;
 --
 -- Database: `payroll`
 --
@@ -20,9 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `members` (
-  `name` varchar(200) NOT NULL,
-  `contact` int(200) NOT NULL,
-  `email` varchar(200) NOT NULL
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pay_method` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -32,12 +42,74 @@ CREATE TABLE `members` (
 --
 
 CREATE TABLE `projects` (
-  `name` varchar(200) NOT NULL,
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `total_cost` int(200) NOT NULL,
-  `member_count` int(200) NOT NULL
+  `total_cost` int(255) NOT NULL,
+  `member_count` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `trans_id` int(255) NOT NULL,
+  `member_id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `pay_date` varchar(255) NOT NULL,
+  `pay_amount` int(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `transaction_no` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`trans_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `trans_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
